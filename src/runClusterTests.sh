3356fe767882f6eb19e71e96b3ab4d66baae8242
Michael Simons <michael@simons.ac>
2021-05-11 18:07:13 +0200

    Try to fix JBang install.

--- src/runClusterTests.sh
+++ src/runClusterTests.sh
@@ -56,4 +56,4 @@ fi
 echo "Starting the cluster tests"
 cd $SRC_DIR
 chmod +x $WORK_DIR/runClusterTests.java && cp logback.xml $WORK_DIR/logback.xml
-jbang $WORK_DIR/runClusterTests.java
+./jbang $WORK_DIR/runClusterTests.java
