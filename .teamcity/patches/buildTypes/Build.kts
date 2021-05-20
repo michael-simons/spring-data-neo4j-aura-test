6551858be8f286f94e2b9531d2601fab28ef1332
johannes.unterstein <johannes.unterstein@TeamCity>
2021-05-20 09:15:31 +0000

    TeamCity change in 'object-graph mapping / Spring Data Neo4j Aura Test' project: parameters of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -13,6 +13,9 @@ changeBuildType(RelativeId("Build")) {
         add {
             param("env.SDN_NEO4J_PASSWORD", "credentialsJSON:00e18f6a-8c39-4d3a-873f-0aa897d6bb77")
         }
+        add {
+            param("env.SDN_NEO4J_URL", "%dep.CloudRoot_Neo4jCloud_Neo4jCloudSetupIntegrationTest.CONNECTIONURL%")
+        }
     }
 
     dependencies {
